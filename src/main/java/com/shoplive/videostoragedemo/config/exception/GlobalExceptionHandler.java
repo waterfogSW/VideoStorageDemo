2f4e16597ef90c7a20afb70fa0bc8701be4f7205
waterfog9580 <waterfog9580@gmail.com>
2023-02-10 15:31:36 +0900

    :sparkles: video multipart tomcat exception handler implement

---
+++ src/main/java/com/shoplive/videostoragedemo/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.shoplive.videostoragedemo.config.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MaxUploadSizeExceededException.class)
+  public ResponseEntity<String> sizeLimitExceed(Exception e) {
+    final var exceptionMessage = e.getMessage();
+    log.info(exceptionMessage);
+    return ResponseEntity.badRequest()
+                         .body(exceptionMessage);
+  }
+
+}
