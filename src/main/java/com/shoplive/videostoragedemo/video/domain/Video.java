eb5b9573ee6b003740c7b80612797c3c30dd9fe9
waterfog9580 <waterfog9580@gmail.com>
2023-02-10 15:39:29 +0900

    :sparkles: implement video domain

---
+++ src/main/java/com/shoplive/videostoragedemo/video/domain/Video.java
@@ -0,0 +1,31 @@
+package com.shoplive.videostoragedemo.video.domain;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+
+@Getter
+public class Video {
+
+  public static final String VIDEO_FORMAT = "video/mp4";
+  public static final Long MAX_VIDEO_SIZE = 1024L * 1024L * 100L;
+
+  private Long id;
+  private final String title;
+  private final long fileSize;
+  private final Path filePath;
+  private final LocalDateTime createdAt;
+
+  public Video(
+      String title,
+      long fileSize,
+      Path filePath
+  ) {
+    this.title = title;
+    this.fileSize = fileSize;
+    this.filePath = filePath;
+    this.createdAt = LocalDateTime.now();
+  }
+
+}
