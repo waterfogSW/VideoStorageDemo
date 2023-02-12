108d4cf7c4ed2f078b10f18fa8db7742db1727aa
waterfog9580 <waterfog9580@gmail.com>
2023-02-12 16:21:25 +0900

    :sparkles: enable async

---
+++ src/main/java/com/shoplive/videostoragedemo/common/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.shoplive.videostoragedemo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+  @Bean
+  public ThreadPoolTaskExecutor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5);
+    executor.setMaxPoolSize(10);
+    executor.setQueueCapacity(25);
+    return executor;
+  }
+
+}
