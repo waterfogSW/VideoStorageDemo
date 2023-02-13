4da1acec5e0232c0b79a47bf1b749f52d93064ba
waterfog9580 <waterfog9580@gmail.com>
2023-02-13 21:05:27 +0900

    :bug: return null if null info pass

--- src/main/java/com/shoplive/videostoragedemo/video/adapter/in/web/dto/VideoMetadataLookupDetailResponse.java
+++ src/main/java/com/shoplive/videostoragedemo/video/adapter/in/web/dto/VideoMetadataLookupDetailResponse.java
@@ -20,7 +20,11 @@ public record VideoMetadataLookupDetailResponse(
         VideoFileInfo videoFileInfo,
         String resourceUrl
     ) {
-      return new VideoFileInfoResponse(videoFileInfo.fileSize(), resourceUrl + videoFileInfo.filePath().getFileName());
+      if (videoFileInfo == null) {
+        return null;
+      }
+      return new VideoFileInfoResponse(videoFileInfo.fileSize(), resourceUrl + videoFileInfo.filePath()
+                                                                                            .getFileName());
     }
 
   }
